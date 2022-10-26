01c3349fd254b7894c1ca8b101f80d78484dcbe7
brayanaldaba <dotawowb@hotmail.com>
2022-10-26 10:14:24 -0400

    first commit 7

--- app/src/main/java/com/example/myapplication/CreateAccountActivity.java
+++ app/src/main/java/com/example/myapplication/CreateAccountActivity.java
@@ -10,5 +10,12 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        shoetoolBar(getString(R.string.title_crear_cuenta), true);
     }
+    public void shoetoolBar(String titulo, boolean botonSubir){
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSuportActioinBar().setTitle(titulo);
+        getSuportActiojnBar().setDisplayHomeAsUpEnabled(botonSubir);
+}
 }
